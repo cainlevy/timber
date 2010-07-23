0ae069e044473d6323942419ba95e2e5b8dc5e05
Lance Ivy <lance@cainlevy.net>
2010-07-23 15:57:30 -0700

    a sample Timber plugin: this adds Oink memory log parsing

---
+++ lib/timber/plugins/oink.rb
@@ -0,0 +1,14 @@
+module Timber
+  module Plugins
+    module Oink
+      # Oink Plugin
+      attr_reader :memory, :pid
+      def memory
+        @_memory ||= @memory ? @memory.to_i : nil
+      end
+    end
+  end
+  
+  Request.include Plugins::Oink
+  Request::PATTERNS[/Memory usage: (\d+) \| PID: (\d+)/] = [:memory, :pid]
+end
