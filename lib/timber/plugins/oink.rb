bf764795f63ab6330e14b18054cd60a0b50abd97
Lance Ivy <lance@cainlevy.net>
2010-07-29 14:20:49 -0700

    support for oink's active-record logging

--- lib/timber/plugins/oink.rb
+++ lib/timber/plugins/oink.rb
@@ -6,9 +6,13 @@ module Timber
       def memory
         @_memory ||= @memory ? @memory.to_i : nil
       end
+      def ar_count
+        @_ar_count ||= @ar_count ? @ar_count.to_i : nil
+      end
     end
   end
   
   Request.class_eval {include Plugins::Oink}
   Request::PATTERNS[/Memory usage: (\d+) \| PID: (\d+)/] = [:memory, :pid]
+  Request::PATTERNS[/Instantiation Breakdown: Total: (\d+)/] = [:ar_count]
 end
