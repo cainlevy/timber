8129b328749140d5ca2b1a7c49328f0bf9ff14a6
Lance Ivy <lance@cainlevy.net>
2010-09-14 11:12:15 -0400

    discarb junk at the top of the file before the first request

--- lib/timber/log.rb
+++ lib/timber/log.rb
@@ -4,6 +4,11 @@ module Timber
       @file = f.is_a?(String) ? File.open(f, 'r') : f
       @line = nil
       @name = File.basename(@file.path).sub(/\..*$/, '')
+
+      # discard any comments or junk at the top of the file
+      until @line.to_s.match(/^Processing/)
+        @line = @file.gets
+      end
     end
     
     attr_reader :current, :name
