7b60d1994c6c324cbfc3d55242c89f3b72d2b912
Lance Ivy <lance@cainlevy.net>
2011-05-05 10:22:10 -0700

    tests against a faked rails 2.3.11 log

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,8 @@
 require 'rubygems'
+require 'test/unit'
+gem 'rails', '2.3.11'
 require 'active_support'
 require 'active_support/test_case'
+
+PLUGIN_ROOT = File.dirname(__FILE__) + '/../'
+ActiveSupport::Dependencies.autoload_paths << File.join(PLUGIN_ROOT, 'lib')
--- test/timber_test.rb
+++ test/timber_test.rb
@@ -1,8 +1,33 @@
-require 'test_helper'
+require 'test/test_helper'
 
 class TimberTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "should name a log file" do
+    @log = Timber::Log.new('test/sample-2-3-11.log')
+    assert_equal 'sample-2-3-11', @log.name
+  end
+
+  test "should find all requests in log file" do
+    @log = Timber::Log.new('test/sample-2-3-11.log')
+    i = 0
+    i += 1 while request = @log.shift
+    assert_equal 4, i
+  end
+
+  test "should represent a request in a log file" do
+    @log = Timber::Log.new('test/sample-2-3-11.log')
+    @request = @log.shift
+    assert_equal 'Foos', @request.controller
+    assert_equal 'redirect', @request.method
+    assert_equal Time.parse('Thu May 05 09:42:48 -0700 2011'), @request.time
+    assert_equal 142, @request.benchmark
+    assert_equal 'http://www.example.com/foos/redirect', @request.url
+    assert_equal 6, @request.lines.count
+  end
+
+  test "should extract oink data" do
+    @log = Timber::Log.new('test/sample-2-3-11.log')
+    @request = @log.shift
+    assert_equal 283212, @request.memory
+    assert_equal "8000", @request.pid
   end
 end
